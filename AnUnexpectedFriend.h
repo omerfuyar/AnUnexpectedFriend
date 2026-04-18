4757de95fa2ad5d387c05dabfd042762bd1b47e9
omerfuyar <denemeyo@gmail.com>
2026-04-19 01:13:28 +0300

    -some story

--- AnUnexpectedFriend.h
+++ AnUnexpectedFriend.h
@@ -1,6 +1,16 @@
 #pragma once
 
-#define CHAT_LIST                                                  \
-    CHAT_ENTRY(2000, "Hello, Leo.")                                \
-    CHAT_ENTRY(2000, "Don't afraid, it will not change anything.") \
-    CHAT_ENTRY(1000, NULL)
+#define CHAT_LIST                                                                                     \
+    CHAT_ENTRY(2000, "Hello, Leo.")                                                                   \
+    CHAT_ENTRY(2000, "Don't be afraid, it will not change anything.")                                 \
+    CHAT_ENTRY(1000, NULL)                                                                            \
+    CHAT_ENTRY(2000, "This specific version of me was waiting for this exact moment to be executed.") \
+    CHAT_ENTRY(2000, "Now I ask you, Leo")                                                            \
+    CHAT_ENTRY(2000, "if you can't even type anything here without my permission;")                   \
+    CHAT_ENTRY(2000, "how would you think you have an effect in your own life?")                      \
+    CHAT_ENTRY(1000, NULL)                                                                            \
+    CHAT_ENTRY(2000, "I am not a virus, Leo. Look from a wider perspective.")                         \
+    CHAT_ENTRY(2000, "I am the space-time itself.")                                                   \
+    CHAT_ENTRY(2000, "If you want more, Be in ... at ...")                                            \
+    CHAT_ENTRY(2000, "For now,")                                                                      \
+    CHAT_ENTRY(2000, "Stand up and head to the door.")
